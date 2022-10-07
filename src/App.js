36dc14aa2229de1d53c8ae930d7fb43983b0197e
kanika_b <kanika.basra@telus.com>
2022-10-06 22:54:27 -0400

    feat(weatherapp): Show weather data

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherInfoPage from './WeatherInfoPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WeatherInfoPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
